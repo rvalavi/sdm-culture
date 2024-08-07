73ebb4a792231ec757c717c0159660d8ccc25476
Valavi, Roozbeh (Environment, Clayton) <Roozbeh.Valavi@csiro.au>
2024-08-07 22:56:19 +1000

    climate data download

--- R/climate_data.R
+++ R/climate_data.R
@@ -1,52 +1,72 @@
 library(magrittr)
 library(terra)
 
-terra::ext(c(50, 150, -10, 50)) %>% 
-  as.polygons() %>% 
-  sf::st_as_sf()
+cat_ext <- terra::ext(c(65, 145, -10, 50)) 
+plant_ext <- terra::ext(c(-100, -80, 12, 24)) 
 
+# periods to download
+periods <- c("1981-2010", "2041-2070/GFDL-ESM4/ssp585")
+# variable of interest
+vars <- c(
+    paste0("bio", 1:19),
+    "gdd10",
+    
+)
 
-months <- 1:12
-years <- 2003:2017
-vars <- c("pr", "tas", "tasmax", "tasmin")
-
-out_path <- "data/CHELSA_data/"
-url_base <- "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V2/GLOBAL/monthly"
-
-# australia <- terra::vect("data/bioregions/Australia_border.shp")
-plot(australia)
-
-for(vr in vars){
-  for(yr in years){
-    for(mn in months){
-      
-      name <- sprintf("CHELSA_%s_%02d_%s_V.2.1.tif", vr, mn, yr)
-      out_name <- paste0(out_path, name)
-      # get the full url for download
-      url_full <- file.path(url_base, vr, name)
-      
-      tryCatch(
-        {
-          download.file(url = url_full, destfile = out_name, method = "curl")
-        },
-        error = function(cond) {
-          message("This data failed:", vy, yr, mn)
-          next
-        }
-      )
-      
-      # read and mask the data
-      r <- terra::rast(out_name) %>% 
-        terra::crop(australia) %>% 
-        terra::mask(
-          mask = australia,
-          filename = file.path(out_path, vr, name)
-        )
-      
-      # remove the main file
-      unlink(out_name)
-      print(out_name)
-    }
-  }
+clean_name <- function(x) {
+    return(
+        tolower(gsub("/", "_", x))
+    )
 }
 
+clean_name("1981-2010")
+clean_name("2041-2070/GFDL-ESM4/ssp585")
+
+"CHELSA_bio11_1981-2010_V.2.1.tif"
+"CHELSA_bio11_2041-2070_gfdl-esm4_ssp585_V.2.1.tif" 
+
+out_path <- "data/CHELSA_data"
+url_base <- "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V2/GLOBAL/climatologies/%s/bio"
+
+
+for (tp in periods) {
+    for (vr in vars) {
+        
+        # get the full url for download
+        url_full <- sprintf("%s/CHELSA_%s_%s_V.2.1.tif", url_base, vr, tp)
+        # NOTE: for download just use the parent dir; then delete original
+        dwn_name <- sprintf("%s/%s.tif", out_path, vr)
+        
+        tryCatch(
+            {
+                download.file(url = url_full, destfile = dwn_name, method = "curl")
+            },
+            error = function(cond) {
+                message("Failed download: ", vr, " of ", tp)
+                next
+            }
+        )
+        
+        # output dir base on the input period
+        out_dir <- file.path(out_path, clean_name(tp))
+        if (!dir.exists(out_dir)) dir.create(out_dir, recursive = TRUE)
+        out_name <- file.path(out_dir, vr)
+        
+        # read and mask the data
+        terra::crop(
+            x = terra::rast(dwn_name), 
+            y = cat_ext,
+            filename = sprintf("%s/%s_%s.tif", out_dir, vr, "cat")
+        )
+        
+        terra::crop(
+            x = terra::rast(dwn_name), 
+            y = plant_ext,
+            filename = sprintf("%s/%s_%s.tif", out_dir, vr, "plant")
+        )
+        
+        # remove the main file
+        unlink(out_name)
+        print(out_name)
+    }
+}
---
+++ R/explore_sp_data.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(sf)
+library(tmap)
+
+data("World")
+
+cat_points <- readRDS("data/species_data_prionailurus_bengalensis.rds")
+nrow(cat_points)
+sum(cat_points$value)
+
+plot(cat_points$longitude, cat_points$latitude)
+plot(World$geometry, add = TRUE)
+cat_points %>% 
+    filter(value == 1) %>% 
+    select(longitude, latitude) %>% 
+    points(col = "red")
+abline(v = 65)
+abline(v = 145)
+
+
+pts <- sf::st_as_sf(cat_points, coords = c("longitude", "latitude"), crs = 4326)
+
+tmap_mode("plot")
+
+
+tm_shape(pts) +
+    tm_dots(size = 0.2, alpha = 0.5) +
+    tm_shape(World) +
+    tm_borders("gray", lwd = .5) +
+    tm_shape(pts[pts$value == 1, ]) +
+    tm_dots(col = "red", size = 0.2, alpha = 0.5)
+
+
+
+plant_points <- readRDS("data/species_data_zamia_polymorpha.rds")
+nrow(plant_points)
+sum(plant_points$value)
+
+
+plot(
+    plant_points$longitude,
+    plant_points$latitude
+)
+plot(World$geometry, add = TRUE)
+plant_points %>% 
+    filter(value == 1) %>% 
+    select(longitude, latitude) %>% 
+    points(col = "red")
+
